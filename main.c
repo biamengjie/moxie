d05913a1f3367b1fc7948436a8b065087059575b
Your Name <you@example.com>
2019-01-17 14:54:24 +0800

    构建程序基本框架

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
-
+char arr[1000];
+int index=0;
 int main(){
 
-    printf("hello world!");
+    printf("jinruminganzifuguanlixitong:\n");
+    while(1){
+        printf("1---tianjiayigeminganzifu:\n");
+        printf("2---shanchuzuihouyigeminganzifu:\n");
+        printf("3---xianshisouyoudeminganzifu:\n");
+        printf("4---tihuanminganzifu:\n");
+        printf("5---chengxutuichu:\n");
+        printf("qingshuruxiangyingdegonggnegnbianhao:\n");
+        int code;
+        scanf("%d",code);
+        if(code==1){
+
+        }
+         if(code==2){
+            
+        }
+         if(code==3){
+            
+        }
+         if(code==4){
+            
+        }
+         if(code==5){
+            
+        }
+    }
 
     return 0;
 
