c03eed718e39c0e39aeca7cc56ba7fbbd1373ca7
Your Name <you@example.com>
2019-01-17 14:58:42 +0800

    增加添加一个敏感字符

--- main.c
+++ main.c
@@ -14,6 +14,16 @@ int main(){
         int code;
         scanf("%d",code);
         if(code==1){
+            printf("tianjiayigeminganzifu:\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index]=m;
+            index++;
+            printf("tianjiachenggong,dianjihuichejixu:\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
 
         }
          if(code==2){
